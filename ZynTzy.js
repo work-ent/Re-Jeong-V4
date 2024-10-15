0a0697d73720c06221d8a27052fbf2bdf9bec834
Re-Jeong01 <rejeongguanxii@gmail.com>
2024-10-15 20:48:59 +0200

    Update ZynTzy.js

--- ZynTzy.js
+++ ZynTzy.js
@@ -1401,9 +1401,9 @@ case 'vv': {
         buffer = Buffer.concat([buffer, chunk])
     }
     if (/video/.test(type)) {
-        return Taira.sendFile(m.chat, buffer, 'media.mp4', msg[type].caption || 'Re-Jeong-V4', m)
+        return zyn.sendFile(m.chat, buffer, 'media.mp4', msg[type].caption || 'Re-Jeong-V4', m)
     } else if (/image/.test(type)) {
-        return Taira.sendFile(m.chat, buffer, 'media.jpg', msg[type].caption || 'Re-Jeong-V4', m)
+        return zyn.sendFile(m.chat, buffer, 'media.jpg', msg[type].caption || 'Re-Jeong-V4', m)
     }
 }
 break
