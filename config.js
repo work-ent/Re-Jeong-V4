faaf1643cf40f09fe0653235a6a51d0a61de8cb0
work-ent <workentcartient@gmail.com>
2024-10-22 00:32:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ global.qris = "https://a.top4top.io/p_319465f4i0.jpeg"
 
 
 // GLOBAL SETTING
-global.owner = "27623649420" //masukin no lu
+global.owner = "254796519826" //masukin no lu
 global.namabot = "Re-Jeong V4"
-global.nomorbot = "27623649420" //masukin no lu
+global.nomorbot = "254796519826" //masukin no lu
 global.namaCreator = "Re-Jeong "
 global.linkyt = "-"
 global.autoJoin = false
